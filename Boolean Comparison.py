f0ce5ccfa46a2a51e7a8763120134d7c8f26ea72
rizkiulia <rizkiulia96@gmail.com>
2021-05-27 19:32:52 +0700

    Create Boolean Comparison.py

---
+++ Boolean Comparison.py
@@ -0,0 +1,11 @@
+# Contoh multiple comparison menggunakan Boolean Comparison 'and'
+result = (3 >= 5) and (6 == 4)
+print(result)
+
+# Contoh multiple comparison menggunakan Boolean Comparison 'or'
+result = (4 != 5) or (2 < 1)
+print(result)
+
+# Contoh multiple comparison menggunakan Comparison 'not'
+result = not (5 + 5 == 10)
+print(result)
