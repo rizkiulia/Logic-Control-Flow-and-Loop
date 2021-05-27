5bc846f94e4a5dcd5315d09931cf06dca3f89e21
rizkiulia <rizkiulia96@gmail.com>
2021-05-27 19:32:21 +0700

    Create Comparison Operator.py

---
+++ Comparison Operator.py
@@ -0,0 +1,15 @@
+# Contoh Comparison dari tipe data string
+fruit = 'grape'
+print(fruit != 'grape')
+
+# Contoh Comparison dari tipe data boolean
+t = True
+f = False
+
+print("t != f: ", t != f)
+
+# Contoh Comparison dari tipe data integer
+x = 9
+y = 7
+
+print("x < y:", x < y)
